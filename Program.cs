52f2c1be50b70be3d6ca985dd78178c1188fef0c
PKCPH <patrick@klingaa.dk>
2022-09-06 09:20:21 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Arrays
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("input binary number: ");
+            var output = BinaryStringToInt(Console.ReadLine());
+            Console.WriteLine(output);
+        }
+
+        static int BinaryStringToInt(string input)
+        {
+            char[] array = input.ToCharArray();
+
+            //reverses the arrays so it start with bit 1 instead of  bit 128
+            Array.Reverse(array);
+         
+            //total sum that will be returned
+            int output = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                //if the bit is turned on
+                if (array[i] == '1')
+                {
+                    //med 2 opløftet i i. f.eks. 2^3 = 8, så hvis 00001000 = 8
+                    output += (int)Math.Pow(2, i);
+                }
+            }
+
+            return output;
+        }
+    }
+}
